80ef34ef47a89b627a2afa6e991cd9da6099e8ab
Umang Sharma <umangsharma0524@gmail.com>
2024-05-01 12:36:18 -0400

    Added Fonts & created Header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import SiteHeader from './components/siteheader';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header>
+        <SiteHeader/>
+      </header>  
+      <header className="App-header">        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          UNDER CONSTRUCTION
         </a>
       </header>
     </div>
---
+++ src/components/siteheader.js
@@ -0,0 +1,73 @@
+//import { AppstoreOutlined, MailOutlined, SettingOutlined } from 'icons';
+import { useState } from 'react';
+import { ConfigProvider, Menu } from 'antd';
+import '../styles.css';
+
+function SiteHeader(props) {
+    
+    //State tracking for which button is currently selected
+    const [current, setCurrent] = useState('home');
+    
+    //TODO: Add page paths as a part of this to pass to buttons
+    const siteItems = [
+        {
+            label: 'Icon',
+            key: 'icon'
+        },
+        {
+            label: (
+                <a href="index.js">Home</a>
+            ),
+            key: 'home'    
+        },
+        {
+            label: 'Contact Me',
+            key: 'contact'
+        },
+        {
+            label: 'Projects',
+            key: 'projs'
+        },
+        {
+            label: 'Internships',
+            key: 'interns'
+        },
+        {
+            label: 'Art',
+            key: 'art'
+        }
+    ];
+    
+    const onButtonClick = (e) => {
+        console.log('Clicked: ', e);
+        setCurrent(e.key);
+    };
+    
+    return <div>
+        <ConfigProvider
+            theme={{
+                components: {
+                    Menu: {
+                        colorBgContainer: '#222222',
+                        colorText: '#FFFFFF',
+                        fontFamily: 'Magdelin',
+                        fontSize: '17px',
+                    }
+                }
+            }}
+        >
+        <Menu
+            class-Name="site-header"
+            onClick={onButtonClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={siteItems}
+            style = {{
+                fontWeight: 300
+            }}
+        />
+        </ConfigProvider>
+    </div>
+}
+
+export default SiteHeader;
