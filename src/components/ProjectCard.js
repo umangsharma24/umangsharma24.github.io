d8629f8882cdc5040df6cbc3024a8a292ffc882a
Umang Sharma <umangsharma0524@gmail.com>
2024-05-14 15:32:54 -0400

    Updated HomePage.js
    Added About Me section & Projects Section as part of #10, and #4. Formatting still required on Project cards to match Design Doc

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,4 @@
+//components/ProjectCard.js
+//TO-DO:
+// Use configprovider to format antd card component to match design document
+// and add them to HomePage.js
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,9 @@
 import SiteHeader from '../components/SiteHeader';
 import Title from '../components/Title';
 import MyFooter from '../components/Footer';
+import AboutMeImage from '../assets/images/about_me_img.jpg'
+import '../styles/HomePage.css'
+import { Card } from "antd";
 
 function HomePage() {
     return (
@@ -12,7 +15,93 @@ function HomePage() {
                 <Title />
             </div>
             <div className="home-content">
-                <h2>ABOUT ME</h2>
+                <div className="about-me" style={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+                    <div className="aboutme-text">
+                        <p style={{
+                            fontFamily: 'Origin',
+                            fontSize: '170px',
+                            margin: '0',
+                            padding: '0'
+                        }}> About Me </p>
+                        <p style={{
+                            fontFamily: 'Magdelin',
+                            fontSize: '25px',
+                            width: '750px',
+                            paddingRight: '130px',
+                            textAlign: 'justify',
+                            marginTop: '0'
+                        }}>
+                            I'm a recent graduate from Purdue University with a Bachelor's of Science in Computer Science,
+                            with minors in English and Mathematics. My education at Purdue University was concentrated
+                            on Software Engineering, giving me strong insights to the principles that make a good developer;
+                             how the industry operates; and to constantly be learning. I am determined, clear-communicator, and 
+                            a creative individual. My eagerness along with my creativity teaches me to approach problems from 
+                            different angles to ensure I can find my way through any obstacle. I often channel my creativity
+                            into other hobbies like water-color painting.
+                        </p>
+                    </div>
+                    <div className="aboutme-img">
+                        <img src={AboutMeImage} height="430" style={{
+                            boxShadow: '2px 2px 15px 0px black'
+                        }} />
+                    </div>
+                </div>
+                <div className="projects">
+                        <div className="projects-heading">
+                            Projects
+                        </div>
+                        <div className="projects-cards">
+                            <Card
+                                className="card"
+                                title="GQL"
+                                extra={<a href="https://github.com/GQL-Project">GitHub</a>}
+                                style={{
+                                    width: '90%',
+                                    fontFamily: 'Origin',
+                                    fontSize: '150px',
+                                }}
+                            >
+                                <p style={{
+                                    fontFamily: 'Magdelin',
+                                    fontSize: '35px',
+                                }}>Relational Database Management software with inbuilt Version Control</p>
+                            </Card>
+                            <Card
+                                className="card"
+                                title="VirtuCards"
+                                extra={<a href="https://github.com/VirtuCard/VirtuCard">GitHub</a>}
+                                style={{
+                                    width: '90%',
+                                    fontFamily: 'Origin',
+                                    fontSize: '150px',
+                                }}
+                            >
+                                <p style={{
+                                    fontFamily: 'Magdelin',
+                                    fontSize: '35px',
+                                }}>Card game platform built in Unity for PC and Android with online multiplayer</p>
+                            </Card>
+                            <Card
+                                className="card"
+                                title="Shell Project"
+                                extra={<a href="https://github.com/umangsharma24/Shell-Project">GitHub</a>}
+                                style={{
+                                    width: '90%',
+                                }}
+                            >
+                                <p style={{
+                                    fontFamily: 'Magdelin',
+                                    fontSize: '35px',
+                                }}>Bash-like terminal application with some custom commands</p>
+                            </Card>
+                        </div>
+                </div>
+                <div className="internships">
+
+                </div>
             </div>
         </div>
     );
